e3a786f9f7d75e46dbca4c031619923250f86133
Macbook <macbook@MacBook-Air-Macbook.local>
2020-03-02 17:46:53 +0500

    css minify

--- gulpfile.js
+++ gulpfile.js
@@ -15,3 +15,11 @@ gulp.task('browser-sync', function() {
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+gulp.task('css', function(){
+    gulp.src('src/css/**/*.css')
+    .pipe(minifyCSS())
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('dist/css'))
+});
