ef6a118837a88d144b2e89cbbe7d8649b3a9c03a
Macbook <macbook@MacBook-Air-Macbook.local>
2020-03-03 13:41:05 +0500

    feat: add header in index.html

--- gulpfile.js
+++ gulpfile.js
@@ -17,9 +17,9 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('css', function(){
-    gulp.src('src/css/**/*.css')
+    gulp.src('css/*.css')
     .pipe(minifyCSS())
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
     .pipe(concat('style.min.css'))
-    .pipe(gulp.dest('dist/css'))
+    .pipe(gulp.dest('css'))
 });
