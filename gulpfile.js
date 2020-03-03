943a72ed92726892b83a5033536df30da2bd7d2f
Macbook <macbook@MacBook-Air-Macbook.local>
2020-03-03 21:52:35 +0500

    feat: add hero in main

--- gulpfile.js
+++ gulpfile.js
@@ -15,11 +15,3 @@ gulp.task('browser-sync', function() {
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
-
-gulp.task('css', function(){
-    gulp.src('css/*.css')
-    .pipe(minifyCSS())
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
-    .pipe(concat('style.min.css'))
-    .pipe(gulp.dest('css'))
-});
